e0d59d635166a37ea64db191b2745d90b0020c4e
manalichovatiya <115995019+manalichovatiya@users.noreply.github.com>
2023-03-09 19:59:04 +0530

    Add files via upload

---
+++ 1D-1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>    
+int main(){  
+    int a[10]; 
+    int i;        	
+  
+    printf("Inter elements in the array :\n");  
+    for(i=0; i<10; i++){  
+	    printf("Element[%d]: ",i);
+        scanf("%d", &a[i]);  
+    }    
+    printf("\nElements in array are: ");  
+    for(i=0; i<10; i++){  
+        printf("%d  ", a[i]);  
+    } 
+    printf("\n");	
+}
